9df70a09d2718d9a0d224457c17c56cb9b55b4f7
Kauê <kauevecchia@hotmail.com>
2025-04-17 01:35:57 -0300

    feat: apply react-hook-form with Controller to budget input

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -1,28 +1,35 @@
+import { useForm, Controller } from "react-hook-form";
 import { Label } from "@radix-ui/react-label";
-import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
 import { DollarSign } from "lucide-react";
 
 export function Budget() {
+  const { control } = useForm();
+
   return (
     <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-semibold">Budget</h1>
-      <div>
-        <form className="flex flex-col gap-2 w-full md:w-1/2">
-          <Label>Enter your current budget</Label>
-          <Input
-            type="number"
-            placeholder="Enter amount, e.g. 1000.25"
-            min={0}
-            step={0.1}
-            className="focus:outline-none border border-transparent focus:border-red"
+      <h1 className="text-3xl font-semibold">Your Budget</h1>
+
+      <form className="flex flex-col gap-4 w-full md:w-1/2 p-4">
+        <div className="flex flex-col gap-2">
+          <Label>Set your current budget</Label>
+
+          <Controller
+            name="budget"
+            control={control}
+            defaultValue={0}
+            render={({ field }) => (
+              <Input type="number" placeholder="Enter budget" {...field} />
+            )}
           />
-          <Button className="cursor-pointer max-w-48 text-left">
-            <DollarSign />
-            Set budget
-          </Button>
-        </form>
-      </div>
+        </div>
+
+        <Button className="cursor-pointer max-w-48 text-left">
+          <DollarSign />
+          Set budget
+        </Button>
+      </form>
     </div>
   );
 }
