89bb895db661a046dc71f4ac892fa145eaddd024
Kauê <kauevecchia@hotmail.com>
2025-04-16 22:37:28 -0300

    feat: add basic structure and styling to budget page

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -1,3 +1,28 @@
+import { Label } from "@radix-ui/react-label";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { DollarSign } from "lucide-react";
+
 export function Budget() {
-  return <h1>Budget</h1>;
+  return (
+    <div className="flex flex-col gap-6">
+      <h1 className="text-3xl font-semibold">Budget</h1>
+      <div>
+        <form className="flex flex-col gap-2 w-full md:w-1/2">
+          <Label>Enter your current budget</Label>
+          <Input
+            type="number"
+            placeholder="Enter amount, e.g. 1000.25"
+            min={0}
+            step={0.1}
+            className="focus:outline-none border border-transparent focus:border-red"
+          />
+          <Button className="cursor-pointer max-w-48 text-left">
+            <DollarSign />
+            Set budget
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
