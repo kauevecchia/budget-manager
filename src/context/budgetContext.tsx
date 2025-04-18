c78f5774af8991125a2dd4f76043591f8736a4d6
Kauê <kauevecchia@hotmail.com>
2025-04-17 23:58:35 -0300

    feat: define transaction type and BudgetContextType interface

--- src/context/budgetContext.tsx
+++ src/context/budgetContext.tsx
@@ -1,5 +1,38 @@
-import { createContext } from "react";
+import { createContext, ReactNode } from "react";
+
+type Transaction = {
+  id: string;
+  description: string;
+  amount: number;
+  transaction: "expense" | "income";
+};
+
+interface BudgetContextType {
+  budget: number;
+  setBudget: (budget: number) => void;
+  transactions: Transaction[];
+  addTransaction: (transaction: Transaction) => void;
+  removeTransaction: (index: number) => void;
+}
 
 export const BudgetContext = createContext({} as BudgetContextType);
 
-export function BudgetProvider() {}
+interface BudgetProviderProps {
+  children: ReactNode;
+}
+
+export function BudgetProvider({ children }: BudgetProviderProps) {
+  return (
+    <BudgetContext.Provider
+      value={{
+        budget: 0,
+        setBudget: () => {},
+        transactions: [],
+        addTransaction: () => {},
+        removeTransaction: () => {},
+      }}
+    >
+      {children}
+    </BudgetContext.Provider>
+  );
+}
