28900f1cfdf27d629f40a795c7c24b6bb086d5b8
Kauê <kauevecchia@hotmail.com>
2025-04-18 01:10:39 -0300

    feat: add functions for handling add and remove transactions

--- src/hooks/useBudgetContext.tsx
+++ src/hooks/useBudgetContext.tsx
@@ -1,9 +1,17 @@
 import { useContext } from "react";
-import { BudgetContext } from "../context/budgetContext";
+import { BudgetContext, Transaction } from "../context/budgetContext";
 
 export function useBudgetContext() {
   const { budget, setBudget, addTransaction, removeTransaction, transactions } =
     useContext(BudgetContext);
+
+  const handleRemoveTransaction = (id: number) => {
+    removeTransaction(id);
+  };
+
+  const handleAddTransaction = (transaction: Transaction) => {
+    addTransaction(transaction);
+  };
 
   return {};
 }
