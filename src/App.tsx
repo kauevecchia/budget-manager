1820187a25d6a613542fe83ee21bc8b32f466d68
Kauê <kauevecchia@hotmail.com>
2025-04-18 01:24:15 -0300

    fix: wrap App with BudgetProvider to provide context globally

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
+import { BudgetProvider } from "./context/budgetContext";
 
 export function App() {
   return (
     <BrowserRouter>
-      <Router />
+      <BudgetProvider>
+        <Router />
+      </BudgetProvider>
     </BrowserRouter>
   );
 }
