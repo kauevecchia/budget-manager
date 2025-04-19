ec40281f248536e08e5b37f48a7717d85b1ed177
Kauê <kauevecchia@hotmail.com>
2025-04-19 01:40:51 -0300

    feat: add basic structure and styling to transactions page

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,9 +1,19 @@
+import { Button } from "../components/ui/button";
+import { CircleArrowRight } from "lucide-react";
+
 export function Transactions() {
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-3xl font-semibold">Transactions</h1>
       <form className="flex flex-col gap-3 p-4 md:w-1/2">
-        {/* Form fields will be added in next commits */}
+        <div className="flex flex-col gap-1"></div>
+        <div className="flex flex-col gap-1"></div>
+        <div className="flex flex-col gap-1"></div>
+
+        <Button type="submit" className="max-w-48 cursor-pointer mt-2">
+          Submit
+          <CircleArrowRight />
+        </Button>
       </form>
     </div>
   );
