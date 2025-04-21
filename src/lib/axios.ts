5aebe21f78b1a79c8c7bf17757bf5914f10892a6
Kauê <kauevecchia@hotmail.com>
2025-04-21 19:01:29 -0300

    chore: change base URL to spreadsheet

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://sheet2api.com/v1/QqiYdeRsC1cf/budgetmanager",
+  baseURL: "https://sheet2api.com/v1/QqiYdeRsC1cf/budget-manager2",
 });
