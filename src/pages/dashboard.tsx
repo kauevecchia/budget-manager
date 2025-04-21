97bc6e8aa2e621456439d9897f2220211abfa7dd
Kauê <kauevecchia@hotmail.com>
2025-04-20 23:51:01 -0300

    feat: add basic structure and styling to dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,3 +1,32 @@
 export function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="flex gap-4">
+        <div>
+          <h1 className="text-2xl">Your current balance</h1>
+          <p className="text-3xl font-semibold"></p>
+        </div>
+        <main className="flex gap-6 p-4">
+          <div className="flex-1"></div>
+          <div className="flex flex-col gap-4 flex-1">
+            <h2 className="text-xl font-semibold">Your transactions</h2>
+          </div>
+        </main>
+      </div>
+      <div className="flex flex-col gap-4">
+        <div>
+          <h1>Titulo teste</h1>
+          <p>paragrafo teste</p>
+        </div>
+        <div>
+          <h1>Titulo teste</h1>
+          <p>paragrafo teste</p>
+        </div>
+        <div>
+          <h1>Titulo teste</h1>
+          <p>paragrafo teste</p>
+        </div>
+      </div>
+    </div>
+  );
 }
