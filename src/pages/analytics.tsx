19a6116a1da36d8eef3b3e65c55d96e9345fae3d
Kauê <kauevecchia@hotmail.com>
2025-04-22 19:21:39 -0300

    feat: add basic structure and styling to analytics page

--- src/pages/analytics.tsx
+++ src/pages/analytics.tsx
@@ -1,3 +1,14 @@
 export function Analytics() {
-  return <h1>Analytics</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold">Analytics</h1>
+      <main className="flex flex-col gap-16 p-4">
+        <div className="mt-4"></div>
+        <div className="flex flex-col gap-4">
+          <h2 className="text-xl font-semibold">Insights</h2>
+          <div className="grid grid-cols-3 gap-4"></div>
+        </div>
+      </main>
+    </div>
+  );
 }
