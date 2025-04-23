75216bf99786b0506bc4ffb7c7253d8a871a3e79
Kauê <kauevecchia@hotmail.com>
2025-04-22 21:54:45 -0300

    feat: configure ThemeProvider for theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./Router";
 import { BudgetProvider } from "./context/budgetContext";
+import { ThemeProvider } from "next-themes";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <BudgetProvider>
-        <Router />
-      </BudgetProvider>
-    </BrowserRouter>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <BrowserRouter>
+        <BudgetProvider>
+          <Router />
+        </BudgetProvider>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
