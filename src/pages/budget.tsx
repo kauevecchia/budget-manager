63a292cd189c2ef3a04d60b56fa1e137a1a504e6
Kauê <kauevecchia@hotmail.com>
2025-04-23 20:30:25 -0300

    feat: add animations to all the pages

--- src/pages/budget.tsx
+++ src/pages/budget.tsx
@@ -11,6 +11,7 @@ import { BudgetContext } from "../context/budgetContext";
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import { motion } from "framer-motion";
 
 type BudgetFormInputs = z.infer<typeof budgetFormSchema>;
 
@@ -34,48 +35,55 @@ export function Budget() {
   }
 
   return (
-    <div className="flex flex-col gap-6">
-      <h1 className="text-3xl font-semibold">Your Budget</h1>
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -20 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="flex flex-col gap-6">
+        <h1 className="text-3xl font-semibold">Your Budget</h1>
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 w-full md:w-1/2 md:p-4"
-      >
-        <div className="flex flex-col gap-2">
-          <Label htmlFor="budget">Set your current budget</Label>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 w-full md:w-1/2 md:p-4"
+        >
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="budget">Set your current budget</Label>
 
-          <Controller
-            name="budget"
-            control={control}
-            defaultValue={0}
-            render={({ field }) => {
-              const formatted =
-                field.value === 0 ? "" : formatCurrency(field.value);
+            <Controller
+              name="budget"
+              control={control}
+              defaultValue={0}
+              render={({ field }) => {
+                const formatted =
+                  field.value === 0 ? "" : formatCurrency(field.value);
 
-              return (
-                <Input
-                  type="text"
-                  id="budget"
-                  placeholder="Enter amount (e.g. $1,000.00)"
-                  value={formatted}
-                  onChange={(e) => {
-                    const raw = parseCurrency(e.target.value);
-                    field.onChange(raw);
-                  }}
-                />
-              );
-            }}
-          />
-          {errors.budget && (
-            <p className="text-sm text-red-500">{errors.budget.message}</p>
-          )}
-        </div>
+                return (
+                  <Input
+                    type="text"
+                    id="budget"
+                    placeholder="Enter amount (e.g. $1,000.00)"
+                    value={formatted}
+                    onChange={(e) => {
+                      const raw = parseCurrency(e.target.value);
+                      field.onChange(raw);
+                    }}
+                  />
+                );
+              }}
+            />
+            {errors.budget && (
+              <p className="text-sm text-red-500">{errors.budget.message}</p>
+            )}
+          </div>
 
-        <Button className="cursor-pointer w-full md:max-w-48 text-left">
-          <DollarSign />
-          Set budget
-        </Button>
-      </form>
-    </div>
+          <Button className="cursor-pointer w-full md:max-w-48 text-left">
+            <DollarSign />
+            Set budget
+          </Button>
+        </form>
+      </div>
+    </motion.div>
   );
 }
