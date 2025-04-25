789053d7ceef8fefefe984992a2ccc723de76266
Kauê <kauevecchia@hotmail.com>
2025-04-25 15:00:28 -0300

    feat: add function to insert budget data into spreadsheet

--- src/utils/sheets.ts
+++ src/utils/sheets.ts
@@ -1,6 +1,11 @@
 import { api } from "../lib/axios";
 import { Transaction } from "../types/transactions";
 
+interface addBudgetToSheetProps {
+  userId: string;
+  budget: number;
+}
+
 export async function addNewRowToSheet(data: Transaction) {
   try {
     const response = await api.post("/Transactions", data, {
@@ -35,6 +40,21 @@ export async function removeRowFromSheet(id: number) {
     return response.data;
   } catch (error) {
     console.error("Error while removing row in spreadsheet:", error);
+    throw error;
+  }
+}
+
+export async function addBudgetToSheet(data: addBudgetToSheetProps) {
+  try {
+    const response = await api.post("/Budget", data, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error while adding new budget to spreadsheet:", error);
     throw error;
   }
 }
