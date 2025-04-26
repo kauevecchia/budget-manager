eb4526ebcb09501980829e844bc840e34744188e
Kauê <kauevecchia@hotmail.com>
2025-04-26 13:17:53 -0300

    fix: add logout logic to MobileMenu component

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -8,8 +8,14 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Menu, LogOut } from "lucide-react";
+import { useBudgetContext } from "@/hooks/useBudgetContext";
+import { toast } from "sonner";
 
 export function MobileMenu() {
+  const { handleSignOut } = useBudgetContext();
+
+  const navigate = useNavigate();
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -31,7 +37,15 @@ export function MobileMenu() {
           <Link to="/analytics">Analytics</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="data-[highlighted]:bg-destructive data-[highlighted]:text-white flex items-center gap-2">
+        <DropdownMenuItem
+          className="data-[highlighted]:bg-destructive data-[highlighted]:text-white flex items-center gap-2"
+          onClick={() => {
+            handleSignOut();
+            localStorage.removeItem("userId");
+            navigate("/login");
+            toast.success("See you next time!");
+          }}
+        >
           Sign Out
           <LogOut className="text-inherit" />
         </DropdownMenuItem>
